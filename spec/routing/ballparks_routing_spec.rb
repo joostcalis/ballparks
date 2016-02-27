21af7f9925cc393470f4c40f067eae9abc6c0507
joostcalis <joost_calis@hotmail.com>
2016-02-27 14:25:37 +0100

    added Rspec routing tests for index and show of ballparks

---
+++ spec/routing/ballparks_routing_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe ProjectsController, type: :routing do
+  describe "routes" do
+    it "routes to #index" do
+      expect(get '/ballparks').to route_to 'ballparks#index'
+    end
+
+    it "routes to #show" do
+      expect(get '/ballparks/2').to route_to 'ballparks#show', id: "2"
+    end
+  end
+end
