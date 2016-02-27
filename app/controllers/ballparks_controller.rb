7c18c7f79a0b603cddf29575f061b288c0b98912
joostcalis <joost_calis@hotmail.com>
2016-02-27 14:50:10 +0100

    added index and show method to ballparks router, plus some rspec controller tests

--- app/controllers/ballparks_controller.rb
+++ app/controllers/ballparks_controller.rb
@@ -1,2 +1,16 @@
 class BallparksController < ApplicationController
+  def index
+    render json: {
+      meta: {
+        count: Ballpark.count,
+        page: 0
+      },
+      ballparks: Ballpark.order(:name)
+    }
+  end
+  
+  def show
+      ballpark = Ballpark.find(params[:id])
+      render json: { ballpark: ballpark}
+  end
 end
--- spec/controllers/ballparks_controller_spec.rb
+++ spec/controllers/ballparks_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe BallparksController, type: :controller do
-
+  describe "#index" do
+    it "has a valid index method" do
+      get :index
+      expect(response).to be_success
+    end
+    it "returns json" do
+      get :index
+      expect(response.header['Content-Type']).to include('application/json')
+    end
+  end
 end
