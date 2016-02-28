594eb14505348fb5f2199b0c87e58f9e842be283
joostcalis <joost_calis@hotmail.com>
2016-02-28 23:59:39 +0100

    added overall_rating to review params

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -22,6 +22,6 @@ class ReviewsController < ApplicationController
   end
 
   def review_params
-      params.require(:review).permit(:concession, :extra_activity_rating, :general_experience, :description)
+      params.require(:review).permit(:concession, :extra_activity_rating, :general_experience, :description, :overall_rating)
   end
 end
