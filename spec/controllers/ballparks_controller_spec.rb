1bfe5dcd0b7ce591e1d9d19554a08595156a8b8e
joostcalis <joost_calis@hotmail.com>
2016-02-28 01:10:35 +0100

    added average method in ballparks controller

--- spec/controllers/ballparks_controller_spec.rb
+++ spec/controllers/ballparks_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe BallparksController, type: :controller do
+  let!(:ballpark) { Ballpark.create! name: "test ballpark", id: 1 }
   describe "#index" do
     it "has a valid index method" do
       get :index
@@ -9,6 +10,13 @@ RSpec.describe BallparksController, type: :controller do
     it "returns json" do
       get :index
       expect(response.header['Content-Type']).to include('application/json')
+    end
+  end
+
+  describe "#show" do
+    it "has a valid show method" do
+      get :show, id: 1
+      expect(response).to be_success
     end
   end
 end
