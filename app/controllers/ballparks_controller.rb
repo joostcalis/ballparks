69bfc95ae4a07353e43647ca0442568bb57380c7
joostcalis <joost_calis@hotmail.com>
2016-03-01 00:40:50 +0100

    indexing based on league

--- app/controllers/ballparks_controller.rb
+++ app/controllers/ballparks_controller.rb
@@ -1,11 +1,24 @@
 class BallparksController < ApplicationController
   def index
+    ballparks = Ballpark.all
+    al_west = get_league(ballparks, "al_west")
+    al_central = get_league(ballparks, "al_central")
+    al_east = get_league(ballparks, "al_east")
+    nl_west = get_league(ballparks, "nl_west")
+    nl_central = get_league(ballparks, "nl_central")
+    nl_east = get_league(ballparks, "nl_east")
+
     render json: {
       meta: {
         count: Ballpark.count,
         page: 0
       },
-      ballparks: Ballpark.order(:name)
+      al_west: al_west,
+      al_central: al_central,
+      al_east: al_east,
+      nl_west: nl_west,
+      nl_central: nl_central,
+      nl_east: nl_east
     }
   end
 
@@ -21,6 +34,7 @@ class BallparksController < ApplicationController
   end
 
   private
+
   def get_average(ballpark)
     reviews = ballpark.reviews
     if reviews.empty?
@@ -30,4 +44,16 @@ class BallparksController < ApplicationController
       avg_rating = (review_sum.to_f / reviews.count).round
     end
   end
+
+  def get_league(ballparks, league)
+    array = []
+    ballparks.each do |ballpark|
+      if ballpark.league == league
+        array << ballpark
+      end
+    end
+    return array
+  end
+
+
 end
