c3047691a310e2be175b8e0bc35f3d68ea0d52d4
joostcalis <joost_calis@hotmail.com>
2016-03-01 13:48:40 +0100

    americal league is in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,19 +7,107 @@ Ballpark.create([
   description: "Yankee Stadium is a stadium, used mainly for baseball, located in The Bronx, New York City. It is the home ballpark for the New York Yankees of Major League Baseball (MLB). The $2.3 billion stadium, built with $1.2 billion in public subsidies, replaced the original Yankee Stadium. It is one block north of the original, on the 24-acre former site of Macombs Dam Park; the 8-acre site of the original stadium is now a public park called Heritage Field. The first game at the new Yankee Stadium was a preseason exhibition game against the Chicago Cubs on April 3, 2009; the Yankees won, 7–4. The first regular-season game was played on April 16, a 10–2 Yankee loss to the Cleveland Indians. The overall price tag makes the new Yankee Stadium the most expensive stadium ever built."
 },
 {
-  name: "Nationals Park",
-  team: "Washington Nationals",
-  league: "nl_east",
-  city: "Washington",
-  description: "Nationals Park is a baseball park located along the Anacostia River in      the Navy Yard neighborhood of Washington, D.C. It is the home ballpark for the Washington Nationals, the city's Major League Baseball franchise. When the Nationals franchise relocated to Washington, D.C., they temporarily played at RFK Stadium until Nationals Park was completed. It is the first LEED-certified green major professional sports stadium in the United States. The facility hosted the 2008 season's first game (in North America), when the Nationals hosted the Atlanta Braves on March 30, 2008 and the first game played there was a collegiate baseball game."
-},
-{
   name: "Fenway Park",
   team: "Boston RedSox",
   league: "al_east",
   city: "Boston",
   description: "Fenway Park is a baseball park located in Boston, Massachusetts, at 4 Yawkey Way near Kenmore Square. Since 1912, it has been the home of the Boston Red Sox, the city's Major League Baseball (MLB) franchise. It is the oldest ballpark in MLB.
   Because of its age and constrained location in Boston's dense Fenway–Kenmore neighborhood, the park has been renovated or expanded many times, resulting in quirky features including The Triangle, Pesky's Pole, and the Green Monster in left field. It is the fourth smallest among MLB ballparks by seating capacity, second smallest by total capacity, and one of eight that cannot accommodate at least 40,000 spectators."
+},
+{
+  name: "Camden Yards",
+  team: "Baltimore Orioles",
+  league: "al_east",
+  city: "Baltimore",
+  description: "Oriole Park at Camden Yards, often referred to simply as Camden Yards or Oriole Park, is a Major League Baseball (MLB) ballpark located in Baltimore, Maryland. Home to the Baltimore Orioles, it is the first of the retro major league ballparks constructed during the 1990s and early 2000s, and remains one of the most highly praised. It was completed in 1992 to replace Memorial Stadium.
+  The park is situated in downtown Baltimore, a few blocks west of the Inner Harbor in the Camden Yards Sports Complex. The Orioles celebrated the ballpark's 20th anniversary during the 2012 season and launched the website CamdenYards20.com as part of the celebration. Historically, Oriole Park at Camden Yards is one of several venues that have carried the Oriole Park name for various Baltimore franchises over the years."
+},
+{
+  name: "Tropicana Field",
+  team: "Tampa Bay Rays",
+  league: "al_east",
+  city: "Tampa Bay",
+  description: "Tropicana Field is a domed stadium in St. Petersburg, Florida, United States, that has been the home of the Tampa Bay Rays of Major League Baseball (MLB) since the team's inaugural season in 1998. The stadium is also used for college football and since December 2008 has been the home of the St. Petersburg Bowl, an annual postseason bowl game. It is currently the only domed stadium in Major League Baseball that is not retractable. Tropicana Field is the smallest MLB stadium by seating capacity with tarp covered, obstructed-view seats. It opened in 1990 and was originally known as the Florida Suncoast Dome until 1993 and as the Thunderdome from 1993–1996."
+},
+{
+  name: "Rogers Centre",
+  team: "Toronto Blue Jays",
+  league: "al_east",
+  city: "Toronto",
+  description: "Rogers Centre (originally named SkyDome) is a multi-purpose stadium in downtown Toronto, Ontario, Canada situated just southwest of the CN Tower near the northern shore of Lake Ontario. Opened in 1989 on the former Railway Lands, it is home to the Toronto Blue Jays of Major League Baseball (MLB). Previously, the stadium served as home to the Toronto Argonauts of the Canadian Football League (CFL) and the Toronto Raptors of the National Basketball Association (NBA). The Buffalo Bills of the National Football League (NFL), played an annual game at the stadium as part of the Bills Toronto Series from 2008 to 2013. While it is primarily a sports venue, it also hosts other large-scale events such as conventions, trade fairs, concerts, travelling carnivals, and monster truck shows.
+  The stadium was renamed Rogers Centre following the purchase of the stadium by Rogers Communications, which also bought the Toronto Blue Jays, in 2005. The venue was noted for being the first stadium to have a fully retractable motorized roof, as well as for the 348-room hotel attached to it, with 70 rooms overlooking the field. It is also the last North American major-league stadium built to accommodate both football and baseball. The stadium served as the site of both the opening and closing ceremonies of the 2015 Pan American Games. During the ceremonies, the site was referred to as the Pan Am Dome (officially as the Pan Am Ceremonies Venue) instead of its normal name."
+},
+{
+  name: "U.S. Cellular Field",
+  team: "Chicago White Sox",
+  league: "al_central",
+  city: "Chicago",
+  description: "U.S. Cellular Field (formerly Comiskey Park) is a baseball park in Chicago, Illinois. It is the home of the Chicago White Sox of Major League Baseball's American League. The park is owned by the Illinois Sports Facilities Authority, but operated by the White Sox. The park opened for the 1991 season, after the White Sox had spent 81 years at the original Comiskey Park. The new park, completed at a cost of $167 million, also opened with the Comiskey Park name, but became U.S. Cellular Field in 2003 after U.S. Cellular bought the naming rights at $68 million over 20 years. It hosted the MLB All-Star Game that same year. Many sportscasters and fans continue to use the name Comiskey Park. Prior to its demolition, the old Comiskey Park was the oldest in-use ballpark in Major League Baseball, a title now held by Fenway Park in Boston. The current public address announcer is Gene Honda, who also serves as the PA announcer for the Chicago Blackhawks.
+  The stadium is situated just to the west of the Dan Ryan Expressway in Chicago's Armour Square neighborhood, adjacent to the more famous neighborhood of Bridgeport. It was built directly across 35th Street from old Comiskey Park, which was demolished to make room for a parking lot that serves the venue. Old Comiskey's home plate location is represented by a marble plaque on the sidewalk next to U.S. Cellular Field and the foul lines are painted in the parking lot. Also, the spectator ramp across 35th Street is designed in such a way (partly curved, partly straight but angling east-northeast) that it echoes the contour of the old first-base grandstand."
+},
+{
+  name: "Progressive Field",
+  team: "Cleveland Indians",
+  league: "al_central",
+  city: "Cleveland",
+  description: "Progressive Field is a baseball park located in downtown Cleveland, Ohio, United States. It is the home field of the Cleveland Indians of Major League Baseball, and together with Quicken Loans Arena, is part of the Gateway Sports and Entertainment Complex. It was ranked as Major League Baseball's best ballpark in a 2008 Sports Illustrated fan opinion poll.
+  The ballpark was named Jacobs Field after team owners Richard and David Jacobs, from its inaugural season in 1994 until 2008, when its name was changed to Progressive Field after its naming rights were purchased by locally based Progressive Insurance Company. The ballpark is referred to as The Jake, based on its original name."
+},
+{
+  name: "Comerica Park",
+  team: "Detroit Tigers",
+  league: "al_central",
+  city: "Detroit",
+  description: "Comerica Park is an open-air ballpark located in Downtown Detroit. It serves as the home of the Detroit Tigers of Major League Baseball, replacing Tiger Stadium in 2000.
+  The park is named after Comerica Bank, which was founded in Detroit and was based there when the park opened. Comerica's headquarters have since been moved to Dallas, though the bank still retains a large presence in Detroit. The stadium's seating capacity is 41,574. There is a Detroit People Mover station about a block from the stadium (at Grand Circus Park). Comerica Park sits on the original site of the Detroit College of Law."
+},
+{
+  name: "Kaufmann Stadium",
+  team: "Kansas City Royals",
+  league: "al_central",
+  city: "Kansas City",
+  description: "Ewing M. Kauffman Stadium, often called Kauffman Stadium, nicknamed The K and formerly known as Royals Stadium, is a baseball park located in Kansas City, Missouri, and home to the Kansas City Royals of Major League Baseball (MLB). Together with Arrowhead Stadium, home of the National Football League's Kansas City Chiefs, it is part of the Truman Sports Complex. Since July 2, 1993, the venue has been known as Kauffman Stadium in honor of the Royals' founding owner, Ewing Kauffman.
+  Kauffman Stadium was built specifically for baseball during an era where building multisport cookie-cutter stadiums were commonplace. It is often held up along with Dodger Stadium in Los Angeles as one of the best examples of modernist stadium design.
+  The 1973 and the 2012 MLB All-Star Games were held at Kauffman Stadium."
+},
+{
+  name: "Target Field",
+  team: "Minnesota Twins",
+  league: "al_central",
+  city: "Minnesota",
+  description: "Target Field is a baseball park located in downtown Minneapolis, Minnesota. It is the home ballpark of the Minnesota Twins, the state's Major League Baseball (MLB) franchise. It is the franchise's sixth ballpark and third in Minnesota. The Twins moved to Target Field for the 2010 Major League Baseball season after 28 seasons at the Hubert H. Humphrey Metrodome. It is the first facility built specifically for the Twins since the team moved to the Twin Cities in 1961; Metropolitan Stadium was built for the Minneapolis Millers five years before the franchise's first season in Minnesota, and the Metrodome was built as a multipurpose stadium for the Twins, Minnesota Vikings, and the University of Minnesota Golden Gophers football team. The 2010 season was the first since 1936 in which the franchise (then known as the Washington Senators) did not share their home stadium with an NFL team. The Twins received the certificate of occupancy from Mortenson Construction on December 22, 2009. The Twins staff moved in on January 4, 2010."
+},
+{
+  name: "Minute Maid Park",
+  team: "Houston Astros",
+  league: "al_west",
+  city: "Houston",
+  description: "Minute Maid Park, previously known as The Ballpark at Union Station, Enron Field, and Astros Field, is a ballpark in Downtown Houston, Texas, United States, that opened in 2000 to house the Houston Astros of Major League Baseball (MLB). The ballpark is Houston's first retractable-roofed stadium, and features a natural grass playing field. The ballpark was built as a replacement of the Astrodome, the first domed sports stadium ever built, which opened in 1965. It is named for soft drink brand Minute Maid, a subsidiary of The Coca-Cola Company, which acquired naming rights in 2002 for $100 million over 30 years. As of 2015, Minute Maid Park has a seating capacity of 41,574, which includes 5,095 club seats and 63 luxury suites.
+  The largest entrance to the park is inside what was once Houston's Union Station, and the left-field side of the stadium features a railway as homage to the site's history. The train moves along a track on top of the length of the exterior wall beyond left field whenever an Astros player hits a home run, and/or the Astros win a game. The engine's coal car is filled with giant oranges in reference to Minute Maid's most famous product, orange juice."
+},
+{
+  name: "Angel Stadium of Anaheim",
+  team: "Anaheim Angels",
+  league: "al_west",
+  city: "Anaheim",
+  description: "Angel Stadium of Anaheim, originally Anaheim Stadium and later Edison International Field of Anaheim, is a modern-style ballpark located in Anaheim, California, United States. Since it opened in 1966 it has served as the home ballpark of the Los Angeles Angels of Anaheim of Major League Baseball, and was also home to the Los Angeles Rams of the National Football League (NFL) from 1980 to 1994. The stadium is often referred to by its unofficial nickname The Big A. It is the fourth-oldest active Major League Baseball stadium, behind Fenway Park, Wrigley Field, and Dodger Stadium. It hosted the 1967, 1989, and 2010 Major League Baseball All-Star Games.
+  Angel Stadium and its surrounding parking lot are roughly bounded by Katella Avenue to the north, the Orange Freeway to the east, Orangewood Avenue to the south, and State College Boulevard to the west. Located near the eastern boundary of the parking lot is the landmark Big A sign and electronic marquee, which originally served as a scoreboard support. The halo located near the top of the 230' tall, 210-ton sign is illuminated following games in which the Angels win (both at home and on the road), which gives rise to the fan expression, Light up the Halo!"
+},
+{
+  name: "O.co Coloseum",
+  team: "Oakland Athletics",
+  league: "al_west",
+  city: "Oakland",
+  description: "O.co Coliseum, originally Oakland–Alameda County Coliseum and commonly known as Oakland Coliseum, is a multi-purpose stadium in Oakland, California, United States, which is home to both the Oakland Athletics of Major League Baseball (MLB) and the Oakland Raiders of the National Football League (NFL). It opened in 1966 and is the only remaining stadium in the United States that is shared by professional football and baseball teams. The Coliseum was also home to some games of the San Jose Earthquakes of Major League Soccer in 2008–2009 and hosted games at the 2009 CONCACAF Gold Cup.
+  The Coliseum has 6,300 club seats, 2,700 of which are available for Athletics games, 143 luxury suites, 125 of which are available for Athletics games, and a variable seating capacity of 35,067 for baseball, 56,057 for football and 63,132 for soccer. In seating capacity, O.co Coliseum is the smallest NFL stadium, and the 2nd smallest MLB stadium.
+  The Oakland–Alameda County Coliseum complex consists of the stadium and the neighboring Oracle Arena."
+},
+{
+  name: "Nationals Park",
+  team: "Washington Nationals",
+  league: "nl_east",
+  city: "Washington",
+  description: "Nationals Park is a baseball park located along the Anacostia River in      the Navy Yard neighborhood of Washington, D.C. It is the home ballpark for the Washington Nationals, the city's Major League Baseball franchise. When the Nationals franchise relocated to Washington, D.C., they temporarily played at RFK Stadium until Nationals Park was completed. It is the first LEED-certified green major professional sports stadium in the United States. The facility hosted the 2008 season's first game (in North America), when the Nationals hosted the Atlanta Braves on March 30, 2008 and the first game played there was a collegiate baseball game."
 },
 {
   name: "Marlins Park",
