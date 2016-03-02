c2a2847d947f0c63b9a99853931f9ee3a5b94f70
joostcalis <joost_calis@hotmail.com>
2016-03-02 10:56:19 +0100

    changed average calculation and json rendering

--- app/controllers/ballparks_controller.rb
+++ app/controllers/ballparks_controller.rb
@@ -13,18 +13,18 @@ class BallparksController < ApplicationController
         count: Ballpark.count,
         page: 0
       },
-      al_west: al_west,
-      al_central: al_central,
-      al_east: al_east,
-      nl_west: nl_west,
-      nl_central: nl_central,
-      nl_east: nl_east
+      al_west: al_west.as_json({:include => :reviews, :methods => :average_rating}),
+      al_central: al_central.as_json({:include => :reviews, :methods => :average_rating}),
+      al_east: al_east.as_json({:include => :reviews, :methods => :average_rating}),
+      nl_west: nl_west.as_json({:include => :reviews, :methods => :average_rating}),
+      nl_central: nl_central.as_json({:include => :reviews, :methods => :average_rating}),
+      nl_east: nl_east.as_json({:include => :reviews, :methods => :average_rating})
     }
   end
 
   def show
       ballpark = Ballpark.find(params[:id])
-      average_rating = get_average(ballpark)
+      average_rating = ballpark.average_rating
       reviews = ballpark.reviews
       render json: {
         ballpark: ballpark,
@@ -35,15 +35,15 @@ class BallparksController < ApplicationController
 
   private
 
-  def get_average(ballpark)
-    reviews = ballpark.reviews
-    if reviews.empty?
-      return 0
-    else
-      review_sum = reviews.inject(0) { |sum, review| sum += review.overall_rating }
-      avg_rating = (review_sum.to_f / reviews.count).round
-    end
-  end
+  # def get_average(ballpark)
+  #   reviews = ballpark.reviews
+  #   if reviews.empty?
+  #     return 0
+  #   else
+  #     review_sum = reviews.inject(0) { |sum, review| sum += review.overall_rating }
+  #     avg_rating = (review_sum.to_f / reviews.count).round
+  #   end
+  # end
 
   def get_league(ballparks, league)
     array = []
--- app/models/ballpark.rb
+++ app/models/ballpark.rb
@@ -1,3 +1,15 @@
 class Ballpark < ActiveRecord::Base
   has_many :reviews
+
+  def has_reviews?
+    self.reviews.present?
+  end
+
+  def average_rating
+    if has_reviews?
+      return self.reviews.average(:overall_rating).round
+    else
+      return 0
+    end
+  end
 end
