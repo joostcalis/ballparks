78bdff875a4900dfe7ee8dab0eb0a1a25f9e730a
joostcalis <joost_calis@hotmail.com>
2016-03-19 01:06:38 +0100

    added specs and functionality for latest reviews, cleaned up sorting of leagues

--- spec/models/ballpark_spec.rb
+++ spec/models/ballpark_spec.rb
@@ -4,4 +4,16 @@ RSpec.describe Ballpark, type: :model do
   describe "check has many reviews relation" do
     it { is_expected.to have_many(:reviews) }
   end
+  describe "it should return arrays based on leagues" do
+      it "gives an array of selected ballparks" do
+        ballpark1 = Ballpark.create!(:league => "al_west")
+        ballpark2 = Ballpark.create!(:league => "al_west")
+        ballpark3 = Ballpark.create!(:league => "al_west")
+        ballpark4 = Ballpark.create!(:league => "al_west")
+        ballpark5 = Ballpark.create!(:league => "nl_west")
+        ballpark6 = Ballpark.create!(:league => "nl_west")
+
+        expect(Ballpark.where("league = 'al_west'")).to eq([ballpark1, ballpark2, ballpark3, ballpark4])
+      end
+  end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -9,4 +9,16 @@ RSpec.describe Review, type: :model do
     it { is_expected.to validate_presence_of(:general_experience) }
     it { is_expected.to validate_presence_of(:ballpark) }
   end
+
+  describe "it should order reviews by id descending" do
+      it "orders reviews by id ascending" do
+        ballpark = Ballpark.create!(:name => "test")
+        review1 = Review.create!(:id => 1, :general_experience => 4, :ballpark => ballpark)
+        review2 = Review.create!(:id => 2, :general_experience => 4, :ballpark => ballpark)
+        review3 = Review.create!(:id => 3, :general_experience => 4, :ballpark => ballpark)
+        review4 = Review.create!(:id => 4, :general_experience => 4, :ballpark => ballpark)
+        review5 = Review.create!(:id => 5, :general_experience => 4, :ballpark => ballpark)
+        expect(Review.order(id: :desc).limit(5)).to eq([review5, review4, review3, review2, review1])
+      end
+  end
 end
